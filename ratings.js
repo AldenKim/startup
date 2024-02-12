95717844a9452aa78767f403414423265324a695
AldenKim <aldenkim.ut@gmail.com>
2024-02-12 13:06:11 -0700

    Add 3rd party API javascript representation

--- ratings.js
+++ ratings.js
@@ -14,11 +14,19 @@ const ratings = new Ratings();
 
 const MAX_MESSAGES = 3;
 const messageQueue = [];
+const newsQueue = [];
 
 function addMessageToQueue(message) {
     messageQueue.push(message);
     if (messageQueue.length > MAX_MESSAGES) {
         messageQueue.shift();
+    }
+}
+
+function addNewsToQueue(news) {
+    newsQueue.push(news);
+    if(newsQueue.length > MAX_MESSAGES) {
+        newsQueue.shift();
     }
 }
 
@@ -34,6 +42,24 @@ function displayMessages() {
     });
 }
 
+function displayNews() {
+    const newsContainer = document.querySelector('.news');
+    newsContainer.innerHTML = ''; 
+
+    newsQueue.forEach(newsItem => {
+        const newsElement = document.createElement('div');
+        newsElement.classList.add('news-notif');
+        newsElement.textContent = newsItem;
+        newsContainer.appendChild(newsElement);
+    });
+}
+
+const sampleNews = [
+    "Ryan Reynolds appears in new superbowl trailer",
+    "LALALAND sequel announced",
+    "Tom Cruise earns a high rating on his new movie"
+];
+
 function generateRandomRating() {
     return Math.floor(Math.random() * 5) + 1;
 }
@@ -43,4 +69,11 @@ setInterval(() => {
     const message = `<span class="user-name">Pam</span> rated a movie ${rating} stars`;
     addMessageToQueue(message); // Add message to the queue
     displayMessages(); // Display messages
+}, 5000);
+
+setInterval(() => {
+    const randomIndex = Math.floor(Math.random() * sampleNews.length);
+    const randomNews = sampleNews[randomIndex];
+    addNewsToQueue(randomNews); // Add news to the queue
+    displayNews(); // Display news
 }, 5000);
