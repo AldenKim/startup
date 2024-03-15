b9a985b9ea1d83696a353f22210b631247075458
AldenKim <aldenkim.ut@gmail.com>
2024-03-15 14:17:18 -0600

    Fix endpoints for new user registration

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
+const DB = require('./database.js');
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
@@ -13,10 +14,28 @@ let users = [
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.use(express.static('public'));
+
+app.set('trust proxy', true);
 
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
+
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await DB.getUser(req.body.username)) {
+        res.status(409).send({ msg: 'Existing user' });
+    } else {
+        const user = await DB.createUser(req.body.username, req.body.password);
+        
+        setAuthCookie(res, user.token);
+
+        res.send({
+            id:user._id,
+        });
+    }
+});
 
 apiRouter.post('/login', (req, res) => {
     const { username, password } = req.body;
@@ -135,6 +154,14 @@ apiRouter.post('/user/:username/watchlist', (req, res) => {
 
     res.json({ message: 'Watchlist updated successfully', user });
 });
+
+function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+      secure: true,
+      httpOnly: true,
+      sameSite: 'strict',
+    });
+  }
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
