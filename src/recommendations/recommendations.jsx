7621965059bfd636694bc4617e9c1a7f059f738d
AldenKim <aldenkim.ut@gmail.com>
2024-04-02 16:31:31 -0600

    creating view components

---
+++ src/recommendations/recommendations.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Recommendations() {
+    return (
+        <main className = 'container-fluid bg-secondary text-center'>
+            <div>recommendations displayed here</div>
+        </main>
+    );
+}
---
+++ src/watchlist/watchlist.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Watchlist() {
+    return (
+        <main className = 'container-fluid bg-secondary text-center'>
+            <div>watchlist displayed here</div>
+        </main>
+    );
+}
